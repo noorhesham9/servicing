81be0a61edc313bd9ec11e61122e5c4a133d94e4
norhs <norhsham02@gmail.com>
2023-08-08 04:06:45 +0300

    mid finish landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Components/header/Header";
+import Landing from "./Components/landing/Landing";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Landing />
+    </>
+  );
 }
 
 export default App;
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -9,7 +9,66 @@ export const textVariant = (delay) => {
       opacity: 1,
       transition: {
         type: "spring",
-        duration: 1.25,
+        duration: 1.5,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const Logolanding = (delay) => {
+  return {
+    hidden: {
+      y: -20,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.5,
+        delay: delay,
+      },
+    },
+  };
+};
+export const SwiperVariant = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.5,
+        delay: delay,
+      },
+    },
+  };
+};
+export const ButtonLAnding = (delay) => {
+  return {
+    hidden: {
+      y: 50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.5,
+        delay: delay,
+      },
+    },
+    exit: {
+      x: -50,
+      opacity: 0,
+      transition: {
+        type: "spring",
+        duration: 1.5,
         delay: delay,
       },
     },
@@ -50,6 +109,54 @@ export const zoomIn = (delay, duration) => {
         type: "tween",
         delay: delay,
         duration: duration,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+export const zoomInlanding1 = (delay) => {
+  return {
+    hidden: {
+      borderRadius: "60% 40% 30% 70%/60% 30% 70%",
+    },
+    show: {
+      borderRadius: "30% 60% 70% 40%/50% 60% 30%",
+      transition: {
+        type: "tween",
+        delay: delay,
+        duration: 2.5,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+export const zoomInlanding2 = (delay) => {
+  return {
+    hidden: {
+      borderRadius: "30% 60% 70% 40%/50% 60% 30%",
+    },
+    show: {
+      borderRadius: "50% 70% 75% 45%/70% 65% 47%",
+      transition: {
+        type: "tween",
+        delay: delay,
+        duration: 2.5,
+        ease: "easeOut",
+      },
+    },
+  };
+};
+export const zoomInlanding3 = (delay) => {
+  return {
+    hidden: {
+      borderRadius: "50% 70% 75% 45%/70% 65% 47%",
+    },
+    show: {
+      borderRadius: "60% 40% 30% 70%/60% 30% 70%",
+      transition: {
+        type: "tween",
+        delay: delay,
+        duration: 2.5,
         ease: "easeOut",
       },
     },
