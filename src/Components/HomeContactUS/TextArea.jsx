727def7a73b04b411088fec580d4ece97156d923
norhs <norhsham02@gmail.com>
2023-08-21 02:16:57 +0300

    finish form color and margin

--- src/Components/HomeContactUS/TextArea.jsx
+++ src/Components/HomeContactUS/TextArea.jsx
@@ -18,13 +18,12 @@ function TextArea(Props) {
             color: "var(--text-color)",
             borderRadius: "10px",
           },
-          // bgcolor: "rgba(0,0,0,.4)",
           m: "15px 0",
           "& .MuiInputLabel-root.Mui-focused": {
             color: "var(--primary-color)",
             fontWeight: "700",
           },
-          "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
+          "& .MuiOutlinedInput-notchedOutline": {
             borderRadius: "10px",
             borderColor: "var(--primary-color)",
           },
@@ -34,12 +33,12 @@ function TextArea(Props) {
               borderRadius: "10px",
             },
 
-          "& .css-dpjnhs-MuiInputBase-root-MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline":
+          "& .MuiInputBase-root.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline":
             {
               borderColor: "var(--third-color) !important",
             },
-          "& .css-14s5rfu-MuiFormLabel-root-MuiInputLabel-root": {
-            fontFamily: "inherit !important",
+          "& .MuiFormLabel-root.MuiInputLabel-root": {
+            fontFamily: "inherit ",
           },
         }}
         label={`${title}`}
